fad72a980aa00c1d0d6f0fa9230fa495cfd4c6ac
Edward Yun <eyun1488@gmail.com>
2016-08-21 22:37:14 -0700

    eddy's commit

--- extension/content.js
+++ extension/content.js
@@ -1,15 +1,32 @@
 InboxSDK.load('1', 'sdk_NameCheck_3598f01e7b').then(function(sdk){
-
+	console.log(sdk);
 	// the SDK has been loaded, now do something with it!
 	sdk.Compose.registerComposeViewHandler(function(composeView){
+		// creating Status Bar
+		var statusBar = composeView.addStatusBar("newStatusBar");
+
+		// create a dom element in js and use that in el
+
+		var moleView = sdk.Widgets.showMoleView({
+			el: 1,
+			title: "Fuck off",
+			minimizedTitleEl: "something new"
+		});
+
+		console.log(moleView);
+
+
+
+
 
 		// a compose view has come into existence, do something with it!
 		composeView.addButton({
 			title: "My Nifty Button!",
-			iconUrl: 'https://lh5.googleusercontent.com/itq66nh65lfCick8cJ-OPuqZ8OUDTIxjCc25dkc4WUT1JG8XG3z6-eboCu63_uDXSqMnLRdlvQ=s128-h128-e365',
+			iconUrl: 'http://orig12.deviantart.net/10b1/f/2011/248/c/1/mtg__dotp_2012_by_mickroz-d48xvu3.png',
 			onClick: function(event) {
-				event.composeView.insertTextIntoBodyAtCursor('Hello World!');
+				// event.composeView.insertTextIntoBodyAtCursor('Hello World!');
 			},
+			hasDropdown: false
 		});
 
 	});
